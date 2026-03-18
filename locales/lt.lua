ac8757833cf59ff7fc06393793a86ef921ef7ca0
iSentrie <xvender7@gmail.com>
2026-03-19 00:27:33 +0200

    Update Lithuanian localization messages

--- locales/lt.lua
+++ locales/lt.lua
@@ -5,11 +5,23 @@ return {
     ["invalidTargetId"] = {msg = "Neteisingas žaidėjo ID.", type = "error"},
     ["playerHealed"] = {msg = "Žaidėjas pagydytas.", type = "success"},
     ["noServerHeal"] = {msg = "Negalite pagydyti serverio.", type = "error"},
-    ["playerStatusFrozen"] = {msg = "Žaidėjas užšaldytas.", type = "success"},
-    ["playerStatusUnfrozen"] = {msg = "Žaidėjas atšaldytas.", type = "success"},
-    ["noServerFreeze"] = {msg = "Negalite sušaldyti serverio.", type = "error"},
+    ["playerStatusFrozen"] = {msg = "Žaidėjo būsena užšaldyta.", type = "success"},
+    ["playerStatusUnfrozen"] = {msg = "Žaidėjo būsena atšaldyta.", type = "success"},
+    ["noServerFreeze"] = {msg = "Negalite užšaldyti serverio.", type = "error"},
+
+    ["stress1"] = {msg = "Jaučiate lengvą stresą.", type = "info"},
+    ["stress2"] = {msg = "Jaučiate vidutinį stresą.", type = "error"},
+    ["stress3"] = {msg = "Jaučiate stiprų stresą.", type = "error"},
+    ["stress4"] = {msg = "Jaučiate labai stiprų stresą.", type = "error"},
+    ["stress5"] = {msg = "Jūsų streso lygis kritinis.", type = "error"},
+
+    ["hunger1"] = {msg = "Skrandis pradeda urgzti.", type = "info"},
+    ["hunger2"] = {msg = "Jaučiatės labai alkanas.", type = "error"},
+    ["hunger3"] = {msg = "Jūs badaujate.", type = "error"},
+    ["hunger4"] = {msg = "Mirštate iš bado.", type = "error"},
 
     -- Dev command stuff
     ["invalidAmount"] = {msg = "Neteisingas kiekis.", type = "error"},
-    ["incorrectAction"] = {msg = "Neteisinga veiksmo įvestis.", type = "error"},
+    ["incorrectAction"] = {msg = "Neteisinga įvestis.", type = "error"},
+    ["statusSaved"] = {msg = "Būsena išsaugota duomenų bazėje.", type = "success"},
 }
